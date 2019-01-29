f4c137aed8c26b346cc456dc43f85fffeb3b5a26
Rhenan Cocev <rhenan_cocev01@hotmail.com>
2019-01-29 20:57:09 -0200

    Organização de código

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var autorizacaoBraspag = require('./autorizacaoBraspag');
 var statusPedido = require('./statusPedido');
 var lucratividade = require('./lucratividade');
 var enviarMensagens = require('./enviarMensagens');
+var funcoes = require('./funcoes');
 const moment = require('moment');
 const env = require('./.env');
 const TelegramBot = require('node-telegram-bot-api');
@@ -19,18 +20,18 @@ bot.onText(/\/pk/, (ctx,match) => {
     const chatId = ctx.chat.id;
     const texto = ctx.text;
     const nome = ctx.from.first_name;
-    var autorizado = autorizacao(PessoasAutorizadas, chatId);
+    var autorizado = funcoes.autorizacao(PessoasAutorizadas, chatId);
     var pk_token = texto.substring(4);
 
     if(pk_token === ''){
        bot.sendMessage(chatId, nome + ", digite o comando /pk + <code>cód da consultora</code>. \n Exemplo: /pk 12345567", { parse_mode: "HTML" })
     }else{
-        if(!isNumber(pk_token)){
+        if(!funcoes.isNumber(pk_token)){
             bot.sendMessage(chatId, nome + ", o texto digitado: " + "<b>" + pk_token + "</b>" + ", não é um cód de CN valido!", { parse_mode: "HTML" });
         } else if (autorizado){
             buscaToken.pk(ctx, bot, pk_token);
         }else{
-            autorizacaoNegada(ctx);
+            funcoes.autorizacaoNegada(ctx);
         }
     }
 });
@@ -39,18 +40,18 @@ bot.onText(/\/lucra/, (ctx, match) => {
     const chatId = ctx.chat.id;
     const texto = ctx.text;
     const nome = ctx.from.first_name;
-    var autorizado = autorizacao(PessoasAutorizadas, chatId);
+    var autorizado = funcoes.autorizacao(PessoasAutorizadas, chatId);
     var lucraPedido = texto.substring(6);
 
     if(lucraPedido === ''){
         bot.sendMessage(chatId, nome + ", digite o comando /lucra + <code>numero do pedido</code>. \n Exemplo: /lucra 12345567", { parse_mode: "HTML" })
      }else{
-         if(!isNumber(lucraPedido)){
+         if(!funcoes.isNumber(lucraPedido)){
              bot.sendMessage(chatId, nome + ", o texto digitado: " + "<b>" + lucraPedido + "</b>" + ", não é um número de pedido válido!", { parse_mode: "HTML" });
          } else if (autorizado){
              lucratividade.lucratividade_pedido(ctx, bot, lucraPedido);
          }else{
-             autorizacaoNegada(ctx);
+            funcoes.autorizacaoNegada(ctx);
          }
      }
 
@@ -60,18 +61,18 @@ bot.onText(/\/status/, (ctx,match) => {
     const chatId = ctx.chat.id;
     const texto = ctx.text;
     const nome = ctx.from.first_name;
-    var autorizado = autorizacao(PessoasAutorizadas, chatId);
+    var autorizado = funcoes.autorizacao(PessoasAutorizadas, chatId);
     var pedido = texto.substring(8);
 
     if(pedido === ''){
         bot.sendMessage(chatId, nome + ", digite o comando /status + <code>numero do pedido</code>. \n Exemplo: /status 123456789", { parse_mode: "HTML" });
     }else {
-        if(!isNumber(pedido)){
+        if(!funcoes.isNumber(pedido)){
             bot.sendMessage(chatId, nome + ", o texto digitado: " + "<b>" + pedido + "</b>" + ", não é um número de pedido válido!", { parse_mode: "HTML" });
         }else if (autorizado){
             statusPedido.status_ped(ctx, bot, pedido);
         } else{
-            autorizacaoNegada(ctx);
+            funcoes.autorizacaoNegada(ctx);
         }
     }
 });
@@ -80,18 +81,18 @@ bot.onText(/\/cartao/, (ctx,match) => {
     const chatId = ctx.chat.id;
     const texto = ctx.text;
     const nome = ctx.from.first_name;
-    var autorizado = autorizacao(PessoasAutorizadas, chatId);
+    var autorizado = funcoes.autorizacao(PessoasAutorizadas, chatId);
     var nm_pedido = texto.substring(7);
 
     if(nm_pedido === ''){
         bot.sendMessage(chatId, nome + ", digite o comando /cartao + <code>numero do pedido</code>. \n Exemplo: /cartao 123456789", { parse_mode: "HTML" });
     }else {
-        if (!isNumber(nm_pedido)){
+        if (!funcoes.isNumber(nm_pedido)){
             bot.sendMessage(chatId, nome + ", o texto digitado: " + "<b>" + nm_pedido + "</b>" + ", não é um número de pedido válido!", { parse_mode: "HTML" }); 
         }else if(autorizado){
             autorizacaoBraspag.braspag(ctx,bot,nm_pedido);
             } else{
-                autorizacaoNegada(ctx);
+                funcoes.autorizacaoNegada(ctx);
             }
     }
 });
@@ -100,14 +101,14 @@ bot.onText(/\/boleto/, (ctx, match) => {
     const chatId = ctx.chat.id;
     const texto = ctx.text;
     const nome = ctx.from.first_name;
-    var autorizado = autorizacao(PessoasAutorizadas, chatId);
+    var autorizado = funcoes.autorizacao(PessoasAutorizadas, chatId);
     var num_pedido = texto.substring(7);
 
     if(num_pedido === ''){
         bot.sendMessage(chatId, nome + ", digite o comando /boleto + <code>numero do pedido</code>. \n Exemplo: /boleto 123456789" , { parse_mode: "HTML" });
     }else {
 
-    if (!isNumber(num_pedido)){
+    if (!funcoes.isNumber(num_pedido)){
         bot.sendMessage(chatId, nome + ", o texto digitado: " + "<b>" + num_pedido + "</b>" + ", não é um número de pedido válido!", { parse_mode: "HTML" });
         
     }/*else {
@@ -116,7 +117,7 @@ bot.onText(/\/boleto/, (ctx, match) => {
     else if(autorizado){
         vencboleto.venc_boleto(ctx, bot, num_pedido);
         } else{
-            autorizacaoNegada(ctx);
+            funcoes.autorizacaoNegada(ctx);
         }
     }
 });
@@ -145,29 +146,6 @@ bot.on('text', (ctx) => {
     }
 
 });
-
-
-// functions:
-function isNumber(n) {
-        return !isNaN(parseFloat(n)) && isFinite(n);
-    } 
-
-function autorizacao(PessoasAutorizadas, chatId){
-    var autorizado = false
-    for (i=0; i<PessoasAutorizadas.length;i++){
-        if (chatId == PessoasAutorizadas[i]){
-            autorizado = true;
-        }
-    }
-    return autorizado;
-}    
-
-function autorizacaoNegada(ctx){
-    const chatId = ctx.chat.id;
-    const nome = ctx.from.first_name;
-    bot.sendMessage(chatId, nome + ", eu não sou autorizado a te responder." 
-    + "\n Qualquer dúvida, acionar o Rhenan da SysMap");
-}
 
 // Note: connections should always be released when not needed
 function doRelease(connection) {
