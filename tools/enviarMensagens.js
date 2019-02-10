8fa479b86afbc5513527f480841cfdbd6f6aa9a0
Rhenan Cocev <rhenan_cocev01@hotmail.com>
2019-02-10 00:26:13 -0200

    inclusao de novas mensagens

--- tools/enviarMensagens.js
+++ tools/enviarMensagens.js
@@ -9,6 +9,7 @@ module.exports = {
     enviarBoasVindas: function(ctx){
     const chatId = ctx.chat.id;
     const nome = ctx.from.first_name + ' ' + ctx.from.last_name;
+
     // send a message to the chat acknowledging receipt of their message
     bot.sendMessage(chatId, 'ChatId: ' + '<code>' + chatId + '</code>' +'\n\n' + 'Olá ' + nome + ', seja Bem-Vindo. Segue os comandos disponíveis:'
         + '\n\n' + 'Se deseja saber a data de cancelamento de um pedido boleto à vista, utilize o comando:'
@@ -20,7 +21,9 @@ module.exports = {
         + '\n\n' + 'Se deseja saber o status de um pedido, utilize o comando:'
         + '\n' + '-> ' + "/status <code>numero do pedido</code>" + '\n Exemplo: /status <code>123456789</code>'
         + '\n\n' + 'Se deseja saber a % de lucratividade de um pedido finalizado, utilize o comando:'
-        + '\n' + '-> ' + "/lucra <code>numero do pedido</code>" + '\n Exemplo: /lucra <code>123456789</code>', {parse_mode: "HTML"});
+        + '\n' + '-> ' + "/lucra <code>numero do pedido</code>" + '\n Exemplo: /lucra <code>123456789</code>'
+        + '\n\n' + 'Se deseja saber a pontuação disponivel para CN captar um pedido, utilize o comando:'
+        + '\n' + '-> ' + "/pts <code>código da consultora</code>" + '\n Exemplo: /pts <code>123456789</code>', {parse_mode: "HTML"});
 
     },
         enviarComandos: function(ctx){
@@ -31,7 +34,8 @@ module.exports = {
             + '\n\n' + '-> ' + "/pk <code>código da consultora</code>" + '\n Exemplo: /pk <code>123456789</code>'
             + '\n\n' + '-> ' + "/cartao <code>numero do pedido</code>" + '\n Exemplo: /cartao <code>123456789</code>'
             + '\n\n' + '-> ' + "/status <code>numero do pedido</code>" + '\n Exemplo: /status <code>123456789</code>'
-            + '\n\n' + '-> ' + "/lucra <code>numero do pedido</code>" + '\n Exemplo: /lucra <code>123456789</code>', {parse_mode: "HTML"});
+            + '\n\n' + '-> ' + "/lucra <code>numero do pedido</code>" + '\n Exemplo: /lucra <code>123456789</code>'
+            + '\n\n' + '-> ' + "/pts <code>código da consultora</code>" + '\n Exemplo: /pts <code>123456789</code>', {parse_mode: "HTML"});
         },
 
         enviarRespostaCasoVazia: function(ctx, x, y){
