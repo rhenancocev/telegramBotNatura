d4d08d942448f4a42d3354589dc1c486834a990e
Rhenan Cocev <rhenan_cocev01@hotmail.com>
2019-02-15 13:04:23 -0200

    inclusao de acesso

--- tokenAcesso/acesso.js
+++ tokenAcesso/acesso.js
@@ -5,8 +5,8 @@ const TelegramBot = require('node-telegram-bot-api');
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(env.token, { polling: false });
 
-var pessoasAutorizadas = [726171735,621550962,461469521,670468351,778923612,747336031,652673739,480623991];
-var PessoasAutorizadasExecutarPedido = [726171735,621550962,480623991,670468351];
+var pessoasAutorizadas = [793933676,726171735,621550962,461469521,670468351,778923612,747336031,652673739,480623991,751654122,717829587];
+var PessoasAutorizadasExecutarPedido = [793933676,726171735,621550962,480623991,670468351,751654122,717829587];
 
 module.exports = {
     pessoasAutorizadas,
